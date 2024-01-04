e1ff5c2c998affb8e16a67457d2c0abf30f02d1e
DESKTOP-C5MKHEH\msh14 <msh1411@naver.com>
2024-01-04 20:00:48 +0900

    민상훈_240104_reactRepo생성, 로그인로직 구현중 민상훈_240104_reactRepo생성, 로그인로직 구현중

--- springRepo/team5final/team5/src/main/java/com/kh/app/member/service/MemberService.java
+++ springRepo/team5final/team5/src/main/java/com/kh/app/member/service/MemberService.java
@@ -76,5 +76,55 @@ public class MemberService {
 		
 		return dao.join(sst,vo);
 	}
+	
+	//회원정보 수정
+	public int edit(MemberVo vo) throws Exception{
+		
+		String pwd = vo.getPwd();
+		String nick = vo.getNick();
+		String phoneNumber = vo.getPhoneNumber();
+		String email = vo.getEmail();
+				
+		
+		//비밀번호 검사
+		Pattern passPattern1 = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$");
+		Matcher passMatcher = passPattern1.matcher(pwd);
+		if(!passMatcher.find()) {
+			throw new Exception("비밀번호는 8~16자 및 대문자와 특수문자를 포함해야해요.");
+		}
+		
+		//닉네임 검사
+		// 닉네임에 "admin"이 포함되는지 대소문자 구별 없이 검사
+		if(nick.toLowerCase().contains("admin")) {
+		    throw new Exception("닉네임에 admin을 포함할 수 없어요.");
+		}
+		// 닉네임의 길이 검사
+		if(nick.length() < 4 || nick.length() > 12) {
+		    throw new Exception("닉네임은 4자 이상 12자 이하이어야 해요.");
+		}
+		
+		// 핸드폰 번호 검사
+		Pattern phonePattern = Pattern.compile("^01[016789]-\\d{3,4}-\\d{4}$");
+		Matcher phoneMatcher = phonePattern.matcher(phoneNumber);
+		if(!phoneMatcher.find()) {
+		    throw new Exception("핸드폰 번호 형식이 올바르지 않습니다.");
+		}
+		
+		// 이메일 검사
+		Pattern emailPattern = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+		Matcher emailMatcher = emailPattern.matcher(email);
+		if(!emailMatcher.find()) {
+			throw new Exception("이메일 주소 형식이 올바르지 않습니다.");
+		}
+		return dao.edit(sst,vo);
+	}
+	//로그인
+	public MemberVo login(MemberVo vo) {
+		return dao.login(sst,vo);
+	}
+	//회원탈퇴
+	public int quit(MemberVo vo) {
+		return dao.quit(sst,vo);
+	}
 
 }
