d3182fe1fe68d6174c95bd78837e4f6d400735a6
oldstudent4312 <x930406@gmail.com>
2024-01-07 23:12:01 +0900

    kty_0107_01 제보 게시글 작성, 제보 게시글 목록 조회 테스트 중

---
+++ springRepo/team5final/team5/src/main/java/com/kh/app/visit/vo/VisitVo.java
@@ -0,0 +1,13 @@
+package com.kh.app.visit.vo;
+
+import lombok.Data;
+
+@Data
+public class VisitVo {
+
+	private String visitNo;
+	private String memberNo;
+	private String reservationDate;
+	private String summitDate;
+	
+}
