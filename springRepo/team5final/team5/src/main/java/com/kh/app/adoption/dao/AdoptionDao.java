a486dea1f13b49bd1f538bb9d5156eec29a55ec0
DESKTOP-52HLHV2\s <katie8454@naver.com>
2024-01-09 09:07:04 +0900

    입양게시판 조금 수정 ~1/9 AM

---
+++ springRepo/team5final/team5/src/main/java/com/kh/app/adoption/dao/AdoptionDao.java
@@ -0,0 +1,16 @@
+package com.kh.app.adoption.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.app.adoption.vo.AdoptionVo;
+
+@Repository
+public class AdoptionDao {
+
+	//입양 게시글 작성
+	public int insert(SqlSessionTemplate sst, AdoptionVo vo) {
+		return sst.insert("AdoptionMapper", vo);
+	}
+
+}
---
+++ springRepo/team5final/team5/src/main/java/com/kh/app/adoption/vo/AdoptionVo.java
@@ -0,0 +1,16 @@
+package com.kh.app.adoption.vo;
+
+import lombok.Data;
+
+@Data
+public class AdoptionVo {
+	
+	private String no;
+	private String imageNo;
+	private String rescueDogNo;
+	private String adminNo;
+	private String enrollDate;
+	private String modifyDate;
+	private String delYn;
+
+}
