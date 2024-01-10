e85cc7491bfe8cbb3c01dc7472c922c88a149b9c
oldstudent4312 <x930406@gmail.com>
2024-01-10 09:09:18 +0900

    kty_0110_01 더미데이터 이미지 5개 업로드, 방문예약 백엔드 완료

--- springRepo/team5final/team5/src/main/java/com/kh/app/visit/service/VisitService.java
+++ springRepo/team5final/team5/src/main/java/com/kh/app/visit/service/VisitService.java
@@ -1,5 +1,51 @@
 package com.kh.app.visit.service;
 
-public class VisitService {
+import java.util.List;
 
-}
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Service;
+
+import com.kh.app.visit.dao.VisitDao;
+import com.kh.app.visit.vo.VisitVo;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class VisitService {
+	
+	private final VisitDao dao;
+	private final SqlSessionTemplate sst;
+	
+	
+	// 예약 목록 조회
+	public List<VisitVo> list() {
+		return dao.list(sst); 
+	}
+
+	// 예약 상세 조회
+	public VisitVo detail(VisitVo vo) {
+		return dao.detail(sst, vo);
+	}
+	
+	// 예약 작성
+	public int insert(VisitVo vo) {
+		return dao.insert(sst, vo);
+	}
+	
+	// 예약 수정
+	public int edit(VisitVo vo) {
+		return dao.edit(sst, vo);
+	}
+	
+	// 예약 취소->삭제
+	public int quit(VisitVo vo) {
+		return dao.quit(sst, vo);
+	}
+	
+	// 상담 완료->삭제
+	public int complete(VisitVo vo) {
+		return dao.complete(sst, vo);
+	}
+
+} // class
--- springRepo/team5final/team5/src/main/java/com/kh/app/visit/vo/VisitVo.java
+++ springRepo/team5final/team5/src/main/java/com/kh/app/visit/vo/VisitVo.java
@@ -7,7 +7,10 @@ public class VisitVo {
 
 	private String visitNo;
 	private String memberNo;
+	private String reservationStatusNo;
+		private String reservationStatus;
 	private String reservationDate;
 	private String summitDate;
+	private String delYn;
 	
 }
