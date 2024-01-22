df13337af42b29ae89d9d2ec004abf72b421183f
oldstudent4312 <x930406@gmail.com>
2024-01-22 18:42:49 +0900

    kty_0122_02 마이페이지에서 예약변경하기 / 예약취소하기 구현중

--- reactRepo/pupple/src/component/Board/visit/VisitMain.jsx
+++ reactRepo/pupple/src/component/Board/visit/VisitMain.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import VisitWrite from './VisitWrite';
 import VisitReservation from './VisitReservation';
+import VisitReservationInfo from './VisitReservationInfo';
 
 const VisitMain = () => {
     return (
         <Routes>
+            <Route path='/reservationInfo' element = {<VisitReservationInfo />}></Route>
             <Route path='/reservation' element = {<VisitReservation />}></Route>
             <Route path='/write' element = {<VisitWrite />}></Route>
         </Routes>
